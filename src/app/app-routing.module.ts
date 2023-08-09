b21256aefef06c8136fbe7d2b5604f527a6b02e7
stevenquintana <steve.e.quint@gmail.com>
2023-08-09 15:10:14 -0700

    Implement preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -9,7 +10,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
