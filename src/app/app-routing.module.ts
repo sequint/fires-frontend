55bf0f5f498028c76f6dc3e50c736665e7fefdff
stevenquintana <steve.e.quint@gmail.com>
2023-08-09 10:29:31 -0700

    Remove home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '/',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
